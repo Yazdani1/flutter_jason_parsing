d635002c33c97d6ca2eb570222d001651fdcc0a5
Yazdani <yaz4dani@gmail.com>
2019-05-17 02:31:37 +0600

    second commit

--- lib/DetailsPage.dart
+++ lib/DetailsPage.dart
@@ -13,16 +13,29 @@ class Detail extends StatelessWidget {
         backgroundColor: Colors.brown,
       ),
 
-      body: new Column(
-        children: <Widget>[
+      body: Container(
+        margin: EdgeInsets.all(15.0),
+        child: new Column(
+          children: <Widget>[
+            new Container(
+              height: 150.0,
+              color: Colors.brown,
+              child: new Card(
+                elevation: 10.0,
+                shape: BeveledRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: new Text(user.title,style: TextStyle(fontSize: 18.0),),
+              ),
+            ),
+            new Image.network(user.url),
+            new SizedBox(height: 10.0,),
 
-          new Image.network(user.url),
-          new SizedBox(height: 10.0,),
-          new Text(user.title,style: TextStyle(fontSize: 18.0),),
-          new SizedBox(height: 10.0,),
-          new Text(user.index.toString(),style: TextStyle(fontSize: 15.0),)
+            new SizedBox(height: 10.0,),
+            new Text(user.index.toString(),style: TextStyle(fontSize: 15.0),)
 
-        ],
+          ],
+        ),
       ),
 
     );
